c6373f24745b515c43bd2ca3b25846fcb084f825
danieljrenfro <djrenfro95@gmail.com>
2020-11-24 23:47:58 -0600

    working movie api with node

--- server.js
+++ server.js
@@ -12,4 +12,50 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan('common'));
 
+function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization') || '';
+
+  if (!authToken) {
+    return res.status(400).send('Request must have Bearer token');
+  }
+
+  if (authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).send('Invalid credentials');
+  }
+  next();
+}
+
+app.use(validateBearerToken);
+
+function handleGetMovies(req, res) {
+  const { genre, country, avg_vote } = req.query;
+  let response = movieData;
+  let numericAvgVote = Number(avg_vote);
+
+  if (avg_vote && !numericAvgVote) {
+    return res.status(400).send('Average vote must be a number');
+  } else if (avg_vote && numericAvgVote) {
+    response = response.filter(movie => movie.avg_vote >= numericAvgVote);
+  }
+
+  if (genre === '' || genre === ' ') {
+    return res.status(400).send('There must be characters in genre query');
+  } else if (genre) {
+    response = response.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()));
+  }
+
+  if (country === '' || country === ' ') {
+    return res.status(400).send('There must be characters in country field');
+  } else if (country) {
+    response = response.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()));
+  }
+
+
+
+  res.json(response);
+}
+
+app.get('/movie', handleGetMovies)
+
 app.listen(8000, () => console.log('Server is listening on port 8000'));
